62093f557e1345e5b22df0299c6b5975f892d690
minhasmrigank <minhasmrigank@gmail.com>
2021-05-27 11:25:46 +0530

    First Hello world program in C

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{    char ch[100];
+    fgets(ch,100,stdin);
+    printf("Hello, World!\n");
+    printf("%s",ch);
+
+    return 0;
+}
